73edfc12d09842d2c55b7f67270a2c3a318caa35
Sylvester <sylvester.das@gmail.com>
2018-12-02 00:03:25 +0530

    Added map API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(18.9067, 72.8147);
 
             var styles = [
                 {
